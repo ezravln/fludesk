9af652e9b46a5febef5f9a42343a6e7624522e60
ezravln <ezravalenne@gmail.com>
2026-08-03 04:31:18 +0700

    feat: setup project structure and pnpm

---
+++ backend/config/connect.ts
@@ -0,0 +1,25 @@
+import { Pool } from "pg"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+export const db = new Pool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+})
+
+export async function connectDatabase() {
+  try {
+    const client = await db.connect()
+
+    console.log("[DB] Connected successfully")
+
+    client.release()
+  } catch (err: any) {
+    console.error("[DB] Failed to connect\n", error)
+    process.exit(1)
+  }
+}
